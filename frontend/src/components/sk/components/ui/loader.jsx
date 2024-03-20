a96b95a53bd9a78d688832e26e194d383928211d
TrickXer <kingkrabby10@gmail.com>
2024-03-20 12:48:15 +0530

    installed lottie-react

--- frontend/src/components/sk/components/ui/loader.jsx
+++ frontend/src/components/sk/components/ui/loader.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Lottie from 'lottie-react'
+import loader from '../../../../assets/WrapIt Loading.json'
+
 
 export default function Loader(props) {
     
 
     return (
-        <div className='absolute top-0 left-0 right-0 h-screen bg-transparent flex justify-center items-center z-30'>
-            <h1 className='text-xl text-white z-30'>Loading...</h1>
+        <div className='absolute top-0 left-0 right-0 h-screen bg-gray-900 flex justify-center items-center z-30'>
+            <Lottie animationData={loader} />
+            <h1 className='text-xl text-gray-400'>Loading...</h1>
         </div>
     )
 }
