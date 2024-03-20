57684fd4916b3b496de668b8d5af6517e4a640ee
TrickXer <kingkrabby10@gmail.com>
2024-03-20 11:37:58 +0530

    minor UI updates

--- frontend/src/components/sk/pages/user/authentication.jsx
+++ frontend/src/components/sk/pages/user/authentication.jsx
@@ -6,7 +6,7 @@ export default function Authentication(props) {
     
 
     return (
-        <div className='flex w-full justify-center items-center'>
+        <div className='flex flex-col flex-auto w-full justify-center items-center'>
             <Outlet />
         </div>
     )
