6a720c0cea9feea57d050b29588044f6cb99a48b
TrickXer <kingkrabby10@gmail.com>
2024-03-22 01:29:01 +0530

    implemented backend authentication in spring security

--- backend/wrapit/src/main/java/com/sk/wrapit/WrapitApplication.java
+++ backend/wrapit/src/main/java/com/sk/wrapit/WrapitApplication.java
@@ -3,7 +3,7 @@ package com.sk.wrapit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @RequestMapping("/wrapit/api/v1")
+
 @SpringBootApplication
 public class WrapitApplication {
 
